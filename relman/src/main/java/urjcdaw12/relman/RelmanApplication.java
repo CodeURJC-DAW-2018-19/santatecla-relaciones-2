2ea389bcb351ff60d9f890b8d5b1b9acc94a0b13
DiegoMzmn <34867731+DiegoMzmn@users.noreply.github.com>
2019-02-08 23:20:13 +0100

    Aplicación generada con Spring Initializr
    Aplicación Maven generada para Spring Boot 2.1.2 con dependencias para Web, DevTools y Mustachem, sin tests.

---
+++ relman/src/main/java/urjcdaw12/relman/RelmanApplication.java
@@ -0,0 +1,14 @@
+package urjcdaw12.relman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelmanApplication.class, args);
+	}
+
+}
+
