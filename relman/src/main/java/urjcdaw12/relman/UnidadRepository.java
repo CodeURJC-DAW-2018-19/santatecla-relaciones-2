60cd393bc4a0520ca05a516f53403fcfa1885d0e
DiegoMzmn <diego_mzmn@hotmail.com>
2019-02-14 15:13:12 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/CodeURJC-DAW-2018-19/santatecla-relaciones-2""
    This reverts commit e94833c0c9ea7f48b1fd86d2a44f1be889b618d1.

---
+++ relman/src/main/java/urjcdaw12/relman/UnidadRepository.java
@@ -0,0 +1,10 @@
+package urjcdaw12.relman;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UnidadRepository extends JpaRepository<Unit,Long>{
+	List<Unit> findByName(String nombre);
+}
