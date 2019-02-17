ef6efbe3ca1698cbdb0b82a6e2915b6da8bed3ce
DiegoMzmn <diego_mzmn@hotmail.com>
2019-02-17 12:22:17 +0100

    Borrado de login/registro de Units

--- relman/src/main/java/urjcdaw12/relman/MainController.java
+++ relman/src/main/java/urjcdaw12/relman/MainController.java
@@ -3,6 +3,7 @@ package urjcdaw12.relman;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +18,10 @@ public class MainController {
 
 	@Autowired
 	private RelationRepository relationRep;
-	
+
 	@Autowired
 	private CardRepository cardRep;
-	
+
 	@Autowired
 	private UserRepository userRep;
 
@@ -33,58 +34,56 @@ public class MainController {
 		return "index";
 	}
 
-
-
 	@RequestMapping("/{unit}")
 	public String openConcreteUnit(Model model, @PathVariable String unit, HttpServletRequest request) {
-			Unit unitConc = unidadRep.findByName(unit).get(0);
-		
-			model.addAttribute("student", request.isUserInRole("USER"));
-			model.addAttribute("teacher", request.isUserInRole("ADMIN"));
-	
-			model.addAttribute("unidad", unidadRep.findByName(unit).get(0));
-			model.addAttribute("padres", relationRep.findByTypeAndDestiny("Herencia", unitConc));
-			model.addAttribute("hijas", relationRep.findByTypeAndOrigin("Herencia", unitConc));
-	
-			model.addAttribute("compuestos",
-					relationRep.findByTypeAndDestiny("Composición", unidadRep.findByName(unit).get(0)));
-			model.addAttribute("partes", relationRep.findByTypeAndOrigin("Composición", unitConc));
-	
-			model.addAttribute("usan", relationRep.findByTypeAndDestiny("Uso",unitConc));
-			model.addAttribute("usa", relationRep.findByTypeAndOrigin("Uso", unitConc));
-	
-			model.addAttribute("asociados a",
-					relationRep.findByTypeAndDestiny("Asociación", unitConc));
-			model.addAttribute("asociado a",
-					relationRep.findByTypeAndOrigin("Asociación", unitConc));
-			
-			model.addAttribute("cards",cardRep.findByUnitAsoc(unitConc));
-		
+
+		Unit unitConc = unidadRep.findByName(unit).get(0);
+
+		model.addAttribute("student", request.isUserInRole("USER"));
+		model.addAttribute("teacher", request.isUserInRole("ADMIN"));
+
+		model.addAttribute("unidad", unidadRep.findByName(unit).get(0));
+		model.addAttribute("padres", relationRep.findByTypeAndDestiny("Herencia", unitConc));
+		model.addAttribute("hijas", relationRep.findByTypeAndOrigin("Herencia", unitConc));
+
+		model.addAttribute("compuestos",
+				relationRep.findByTypeAndDestiny("Composición", unidadRep.findByName(unit).get(0)));
+		model.addAttribute("partes", relationRep.findByTypeAndOrigin("Composición", unitConc));
+
+		model.addAttribute("usan", relationRep.findByTypeAndDestiny("Uso", unitConc));
+		model.addAttribute("usa", relationRep.findByTypeAndOrigin("Uso", unitConc));
+
+		model.addAttribute("asociados a", relationRep.findByTypeAndDestiny("Asociación", unitConc));
+		model.addAttribute("asociado a", relationRep.findByTypeAndOrigin("Asociación", unitConc));
+
+		model.addAttribute("cards", cardRep.findByUnitAsoc(unitConc));
+
 		return "units";
 	}
-	
-	
+
 	@RequestMapping("/register")
-	public String register(@RequestParam("userInput") String user, @RequestParam("pass1") String pass1, @RequestParam("pass2") String pass2) {
+	public String register(@RequestParam("userInput") String user, @RequestParam("pass1") String pass1,
+			@RequestParam("pass2") String pass2) {
 		if (pass1.equals(pass2)) {
-			userRep.save(new User (user,pass1,"ROLE_USER"));
+			userRep.save(new User(user, pass1, "ROLE_USER"));
 		}
-		
+
 		return "redirect:/";
 	}
-	
-	//Ahora mismo funciona perfectamente, pero habría que meter un modal de confirmación de borrado, actualmente refresca la pagina actualizada		
+
+	// Ahora mismo funciona perfectamente, pero habría que meter un modal de
+	// confirmación de borrado, actualmente refresca la pagina actualizada
 	@RequestMapping("/delete/{unit}")
-	public String deleteUnit(Model model,@PathVariable String unit) {
+	public String deleteUnit(Model model, @PathVariable String unit) {
 		Unit unitConc = unidadRep.findByName(unit).get(0);
-		
-		if (unitConc!=null) {
+
+		if (unitConc != null) {
 			unidadRep.delete(unitConc);
-			
+
 		}
-		
-		return "redirect:/"; 
-		
+
+		return "redirect:/";
+
 	}
-	
+
 }
--- relman/src/main/java/urjcdaw12/relman/SecurityConfiguration.java
+++ relman/src/main/java/urjcdaw12/relman/SecurityConfiguration.java
@@ -7,34 +7,34 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public UserRepositoryAuthenticationProvider authenticationProvider;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.headers().frameOptions().disable(); // Para que vaya la consola H2
 
-		
 		http.authorizeRequests().antMatchers("/").permitAll();
 		http.authorizeRequests().antMatchers("/error").permitAll();
-		
-		//http.authorizeRequests().antMatchers("/**").hasAnyRole("USER");
-		http.authorizeRequests().anyRequest().permitAll();//TODO Solo para pruebas
-		
+		http.authorizeRequests().antMatchers("/logout").permitAll();
+		// http.authorizeRequests().antMatchers("/*").hasAnyRole("USER");
+
+		http.authorizeRequests().anyRequest().permitAll();// TODO Solo para pruebas
+
 		http.formLogin().loginPage("/");
 		http.formLogin().usernameParameter("username");
 		http.formLogin().passwordParameter("password");
 		http.formLogin().defaultSuccessUrl("/");
 		http.formLogin().failureUrl("/error");
-		
-		http.logout().logoutUrl("/logout"); 
+
+		http.logout().logoutUrl("/logout");
 		http.logout().logoutSuccessUrl("/");
-		
-		http.csrf().disable(); //TODO Usar tokens
+
+		http.csrf().disable(); // TODO Usar tokens
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider);
