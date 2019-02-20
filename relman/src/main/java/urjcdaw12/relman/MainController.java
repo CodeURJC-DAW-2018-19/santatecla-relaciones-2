08e661429f1efa9ba25a2886263485ad51a80f73
chicocoriano <>
2019-02-20 12:24:27 +0100

    Revert "Implementación de Handler"
    This reverts commit 19ed9a89c0ec57e446761a5c02131e0e8f4b8219.

--- relman/src/main/java/urjcdaw12/relman/MainController.java
+++ relman/src/main/java/urjcdaw12/relman/MainController.java
@@ -3,11 +3,13 @@ package urjcdaw12.relman;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import urjcdaw12.relman.Units.Unit;
 import urjcdaw12.relman.Units.UnitService;
 import urjcdaw12.relman.Users.User;
@@ -28,6 +30,8 @@ public class MainController {
 
 	@RequestMapping("/")
 	public String cargar(Model model, HttpServletRequest request) {
+		CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+		model.addAttribute("token", token.getToken());
 
 		model.addAttribute("units", userComponent.getTabs());
 
@@ -40,7 +44,9 @@ public class MainController {
 
 	@RequestMapping("/register")
 	public String register(Model model, @RequestParam("userInput") String user, @RequestParam("pass1") String pass1, @RequestParam("pass2") String pass2, HttpServletRequest request) {
-	
+		CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+		model.addAttribute("token", token.getToken());
+
 		if (pass1.equals(pass2)) {
 			userServ.save(new User(user, pass1, "ROLE_USER"));
 		}
@@ -51,6 +57,8 @@ public class MainController {
 	@RequestMapping("/delete/{unit}")
 	public String deleteUnit(Model model, @PathVariable String unit, HttpServletRequest request) {
 
+		CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+		model.addAttribute("token", token.getToken());
 
 		userComponent.removeTab(unit);
 		
@@ -66,6 +74,8 @@ public class MainController {
 
 	@RequestMapping("/addUnit")
 	public String addUnit(Model model, @RequestParam String newUnit, HttpServletRequest request) {
+		CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+		model.addAttribute("token", token.getToken());
 
 		unitServ.save(new Unit(newUnit));
 
@@ -74,7 +84,9 @@ public class MainController {
 	
 	@RequestMapping("/removeTab/{unit}")
 	public String removeTab(Model model, HttpServletRequest request, @PathVariable String unit) {
-	
+		CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+		model.addAttribute("token", token.getToken());
+		
 		userComponent.removeTab(unit);
 		
 		return "redirect:/";
