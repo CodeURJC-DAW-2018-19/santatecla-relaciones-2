edb4ea22ce51b6e2206ab40618729a8a672563c9
KikeAjani <kikevalps3@gmail.com>
2019-02-23 21:00:39 +0100

    HTTPS + JS separado
    PUERTO CAMBIADO
    
    Co-Authored-By: ogomezr <ogomezr@users.noreply.github.com>

---
+++ relman/target/classes/static/assets/js/ajaxPag.js
@@ -0,0 +1,21 @@
+	function ajaxPag(id) {
+			$(document).ready(function(){	
+  				 var p= document.getElementById(id).value;
+  				 var name =document.getElementById(id).name;
+  				 console.log(name);
+  	 		 	 $.ajax({url: "/"+name+"/"+ p +"/10", success: function(result){
+  	 		 	 	var suma = parseInt(p) + parseInt(1);
+  	 		 	 	document.getElementById(id).value= suma ; 
+  	 		 	 	console.log(suma);
+   	  			    var d1 = document.getElementById(id+name);
+					d1.insertAdjacentHTML('beforeend', result);
+					var n =document.getElementById(id).title;
+					document.getElementById(id).title = parseInt(n) - parseInt(10); 
+					var c = document.getElementById(id).title;
+					if( c <=0 ){
+					document.getElementById(id).disabled = true;
+					document.getElementById(id).style.display = 'none';
+					}	
+   	 			}});	 		
+		});
+		}
