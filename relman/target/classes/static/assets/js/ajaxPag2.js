f7be5a96a18bfd6ec4b6c129fe15465f822917d2
KikeAjani <kikevalps3@gmail.com>
2019-02-24 20:55:07 +0100

    Fix CSS en error.mustache

--- relman/target/classes/static/assets/js/ajaxPag2.js
+++ relman/target/classes/static/assets/js/ajaxPag2.js
@@ -1,24 +1,22 @@
-	function ajaxPag2(id) {
-	            $(document).ready(function(){
-	                   var p= document.getElementById(id).value;
-	                   var rel =document.getElementById(id).name;
-	                   var unidad =document.getElementById("unit").value;
-	                   console.log(rel);
-	                   console.log(unidad);
-	                     $.ajax({url: "/rel/"+unidad+"/"+rel+"/"+p+"/", success: function(result){
-	                         console.log(result);
-	                         var suma = parseInt(p) + parseInt(1);
-	                         document.getElementById(id).value= suma ; 
-	                         console.log(suma);
-	                         var d1 = document.getElementById(id+rel);
-	                    d1.insertAdjacentHTML('beforeend', result);
-	                    var n =document.getElementById(id).title;
-	                    document.getElementById(id).title = parseInt(n) - parseInt(5); 
-	                    var c = document.getElementById(id).title;
-	                    if( c <=0 ){
-	                    document.getElementById(id).disabled = true;
-	                    document.getElementById(id).style.display = 'none';
-	                    }
-	                    }});
-	        });
-	        }
+function ajaxPag2(id) {
+                $(document).ready(function(){
+                       var p= document.getElementById(id).value;
+                       var rel =document.getElementById(id).name;
+                       var unidad =document.getElementById("unit").value;
+                       $('#'+id+'spinner').html("<img src='images/spinner.gif' width='50em'/>");
+                         $.ajax({url: "/rel/"+unidad+"/"+rel+"/"+p+"/", success: function(result){
+                             var suma = parseInt(p) + parseInt(1);
+                             document.getElementById(id).value= suma ; 
+                             var d1 = document.getElementById(id+rel);
+                             d1.insertAdjacentHTML('beforeend', result);
+                             var n =document.getElementById(id).title;
+                             document.getElementById(id).title = parseInt(n) - parseInt(5); 
+                             var c = document.getElementById(id).title;
+                             if( c <=0 ){
+                                 document.getElementById(id).disabled = true;
+                                 document.getElementById(id).style.display = 'none';
+                             }
+                             $('#'+id+'spinner').html("");
+                        }});
+            });
+            }
