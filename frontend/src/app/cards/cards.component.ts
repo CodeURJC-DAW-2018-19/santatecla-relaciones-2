e707fc3515164cc691945728a297ead21620e803
Jesús <chechuhorcajo@outlook.com>
2019-03-30 18:48:09 +0100

    Nuevos elementos de diseño

--- frontend/src/app/cards/cards.component.ts
+++ frontend/src/app/cards/cards.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry, MatDialog } from '@angular/material';
+
+import { ChangeDetectorRef, AfterViewInit, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+  @ViewChild('close') private cerrar: TemplateRef<object>;
+  @ViewChild('dialogContent') private template: TemplateRef<object>;
 
-  constructor() { }
+  
+  
+  config = {
+    width: '50%',
+    height: '50%',
+  };
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  dialogCerrar() {
+    this.dialog.open(this.cerrar, this.config);
+  }
+
+  openTemplate() {
+    this.dialog.open(this.template, this.config);
   }
 
 }
