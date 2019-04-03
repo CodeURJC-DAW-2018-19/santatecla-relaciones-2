e602aa42f3ed91914fd038a9e8b8af8830a01163
DiegoMzmn <diego_mzmn@hotmail.com>
2019-04-03 20:38:13 +0200

    Avances en card component
    Ahora se muestran las cards, están paginadas y se distingue funcionalidad por roles (aunque la funcionalidad en si no está implementada).

--- frontend/src/app/cards/card.component.ts
+++ frontend/src/app/cards/card.component.ts
@@ -3,45 +3,51 @@ import { ActivatedRoute } from '@angular/router';
 import { Card } from './card.model';
 import { Page } from '../page.module';
 import { CardService } from './card.service';
+import { LoginService } from '../login/login.service';
 
 @Component({
-    selector: 'app-component',
-    templateUrl: './card.component.html',
-    styleUrls: ['./cards.component.css'],
-  })
+  selector: 'app-component',
+  templateUrl: './card.component.html',
+  styleUrls: ['./cards.component.css'],
+})
 
-  export class CardComponent implements OnInit{
-    unitName:string;
-    activeLinkIndex=-1;
-    cards: Card[];
-    lastRequestedPage:Page;
-    pageNumber:number;
-    
-    constructor(private activeRoute:ActivatedRoute, private service:CardService){
-      this.unitName = this.activeRoute.snapshot.params.name;
-    }
+export class CardComponent implements OnInit {
+  unitName: string;
+  activeLinkIndex = -1;
+  cards: Card[];
+  lastRequestedPage: Page;
+  pageNumber: number;
 
-    ngOnInit(): void {
-      this.pageNumber = 0;
-      this.getPage();
-    }
-
-    requestNextPage() {
-      this.pageNumber++;
-      this.getPage();
-    }
-
-    getPage() {
-      this.service.getCards(this.unitName, this.pageNumber).subscribe(
-        page => {
-          this.lastRequestedPage = page;
-          if(this.pageNumber === 0) {
-            this.cards = page.content;
-          } else {
-            this.cards = this.cards.concat(page.content);
-          }
-        },
-        error => console.log(error)
-      );
-    }
+  constructor(private activeRoute: ActivatedRoute, private service: CardService, private loginService: LoginService) {
+    this.unitName = this.activeRoute.snapshot.params.name;
   }
+
+  ngOnInit(): void {
+    this.pageNumber = 0;
+    this.getPage();
+  }
+
+  requestNextPage() {
+    this.pageNumber++;
+    this.getPage();
+  }
+
+  getPage() {
+    this.service.getCards(this.unitName, this.pageNumber).subscribe(
+      page => {
+        this.lastRequestedPage = page;
+        if (this.pageNumber === 0) {
+          this.cards = page.content;
+        } else {
+          this.cards = this.cards.concat(page.content);
+        }
+      },
+      error => console.log(error)
+    );
+  }
+
+  saveChanges(type: string) {
+    //TODO
+    console.log(type);
+  }
+}
--- relman/src/main/java/urjcdaw11/relman/api/LoginController.java
+++ relman/src/main/java/urjcdaw11/relman/api/LoginController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import urjcdaw11.relman.users.User;
