4efa91e64dcca92906d1681bfe090eaed4c12b5e
kinves <oscargomezramirez@gmail.com>
2019-04-06 01:20:03 +0200

    script docker

--- docker/generate_image.sh
+++ docker/generate_image.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
-
+cd $HOME/santatecla-relaciones-2/frontend
+docker run --rm --name angular-cli -v ${PWD}:/frontend -w /frontend node:8.15.1 /bin/bash -c "npm install -g @angular/cli; npm install; npm rebuild; ng build --prod --baseHref=/new/"
+cd dist
+mv my-app $HOME/santatecla-relaciones-2/relman/src/main/resources/static
+cd $HOME/santatecla-relaciones-2/relman/src/main/resources/static
+sudo rm -r new
+mv my-app new
 cd $HOME/santatecla-relaciones-2/relman
 sudo docker run -it --rm --name my-maven-project -v "$(pwd)":/relman -w /relman maven:3.3-jdk-8 mvn install
 
@@ -8,5 +14,4 @@ sudo docker run -it --rm --name my-maven-project -v "$(pwd)":/relman -w /relman 
 #sudo docker login
 #sudo docker push ogomezr/relmanapp
 cd
-
 
